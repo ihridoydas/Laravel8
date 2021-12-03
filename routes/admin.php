c291dcd36b4fd797b405e74e27be5cd9b715119b
Hridoy Das <>
2021-12-03 19:56:57 +0900

    PHP Laravel8 Project 1st commit

---
+++ routes/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/admin-login', [App\Http\Controllers\Auth\LoginController::class, 'adminLogin'])->name('admin.login');
+
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')
+->middleware('is_admin');
+
+
+
+
